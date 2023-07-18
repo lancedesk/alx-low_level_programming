1e40f4a177f56e18118b0665c84acf464fc9afac
Lance Desk <lncedesk.com>
2023-07-18 08:20:24 -0700

    A function that adds two integers and returns the result.

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,18 +2,20 @@
 #define MAIN_H
 
 /**
-* times_table - prints the 9 times table, starting with 0.
-* jack_bauer - prints every minute of the day of Jack Bauer
-* print_last_digit - prints the last digit.
-* int _abs - computes absolute value of an integer.
-* print_sign - prints the sign of a number
-* _isalpha - checks for alphabetic character.
-* int _islower - checks for lowercase character.
-* print_alphabet_x10 - function to print 10 times
-* print_alphabet - function to print small letters
-* _putchar - function to print single characters
+ * add - adds two integers and returns the result.
+ * times_table - prints the 9 times table, starting with 0.
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * print_last_digit - prints the last digit.
+ * int _abs - computes absolute value of an integer.
+ * print_sign - prints the sign of a number
+ * _isalpha - checks for alphabetic character.
+ * int _islower - checks for lowercase character.
+ * print_alphabet_x10 - function to print 10 times
+ * print_alphabet - function to print small letters
+ * _putchar - function to print single characters
 */
 
+int add(int, int);
 void times_table(void);
 void jack_bauer(void);
 int print_last_digit(int);
