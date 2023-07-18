5a0c2a4af1df00e8d9ec93b99d47fb849225fefb
Lance Desk <lncedesk.com>
2023-07-18 05:00:18 -0700

    A function that prints the sign of a number.

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 /**
+* print_sign - prints the sign of a number
 * _isalpha - checks for alphabetic character.
 * int _islower - checks for lowercase character.
 * print_alphabet_x10 - function to print 10 times
@@ -9,6 +10,7 @@
 * _putchar - function to print single characters
 */
 
+int print_sign(int n);
 int _isalpha(int c);
 int _islower(int c);
 void print_alphabet_x10(void);
