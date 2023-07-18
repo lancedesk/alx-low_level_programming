1e7dc766e850808c4ab944ec117f95ac748eed53
Lance Desk <lncedesk.com>
2023-07-18 04:11:37 -0700

    A function that checks for lowercase character.

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _islower - checks if variable islower
+ * @c: character input
+ * Return: 1 for islower and 0 for !
+ */
+
+int _islower(int c)
+{
+if (islower(c))
+{
+	return (1);
+}
+else
+{
+	return (0);
+}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,11 +2,13 @@
 #define MAIN_H
 
 /**
+* int _islower - checks for lowercase character.
 * print_alphabet_x10 - function to print 10 times
 * print_alphabet - function to print small letters
 * _putchar - function to print single characters
 */
 
+int _islower(int c);
 void print_alphabet_x10(void);
 void print_alphabet(void);
 int _putchar(char c);
