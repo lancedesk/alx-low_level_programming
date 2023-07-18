5c87f18cb70dd7f44d14fe2b382c9963b55b178e
Lance Desk <lncedesk.com>
2023-07-18 07:59:00 -0700

    A function that prints the 9 times table, starting with 0.

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,35 +1,33 @@
 #include "main.h"
 /**
- * times_table - prints times table
+ * times_table - prints the 9 times table, starting with 0
  */
-
 void times_table(void)
 {
 	int i, j, result;
-
-for (i = 0; i < 10; i++)
-{
-for (j = 0; j < 10; j++)
-{
-result = i * j;
-if (j != 0)
-{
-	_putchar(',');
-	_putchar(' ');
-if (result < 10)
-	_putchar(' ');
-}
-if (result >= 10)
-{
-	_putchar(result / 10 + '0');
-	_putchar(result % 10 + '0');
-}
-else
-{
-	_putchar(' ');
-	_putchar(result + '0');
-}
-}
-	_putchar('\n');
-}
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			result = j * i;
+			if (j == 0)
+			{
+				_putchar(result + '0');
+			}
+			if (result < 10 && j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(result + '0');
+			} else if (result >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
 }
