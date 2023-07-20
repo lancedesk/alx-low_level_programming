1d17ec29f842bbdfbfe238831d9dbd3bf46fe372
Lance Desk <lncedesk.com>
2023-07-20 05:22:14 -0700

    A function that prints the numbers, from 0 to 9, followed by a new line.

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_numbers - function to print numbers
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == 2 || i == 4)
+		{
+			continue;
+		}
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
+
+
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <ctype.h>
 
+void print_most_numbers(void);
 void print_numbers(void);
 int mul(int a, int b);
 int _isdigit(int c);
