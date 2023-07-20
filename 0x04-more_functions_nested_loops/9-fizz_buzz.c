0862b68acc26e37c8fb85c62bcb1474e253b8100
Lance Desk <lncedesk.com>
2023-07-20 09:00:21 -0700

    A program that prints the numbers from 1 to 100, followed by a new line

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	fizz_buzz();
+	return (0);
+}
+
+/**
+ * fizz_buzz - prints Fizz Fizzbuzz
+ */
+
+void fizz_buzz(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3) == 0 && (i % 5) == 0)
+		{
+			printf("FizzBuzz ");
+			continue;
+		}
+
+		else if ((i % 3) == 0)
+		{
+			printf("Fizz ");
+			continue;
+		}
+
+		else if ((i % 5) == 0)
+		{
+			printf("Buzz ");
+			continue;
+		}
+
+		printf("%d ", i);
+	}
+	putchar('\n');
+}
