ea486352d5abf7e2ce929cbd4643cb83dae59e2e
Lance Desk <lncedesk.com>
2023-07-20 04:02:47 -0700

    More C functions and nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * int _isupper - tests if uppercase
+ * @c: integer input
+ * Return: returns 1 if uppercase
+ * and 0 if lowercase
+ */
+
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
