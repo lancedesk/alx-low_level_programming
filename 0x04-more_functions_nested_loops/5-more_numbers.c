adb312be812b1848faeb31753f0828adb17110a1
Lance Desk <lncedesk.com>
2023-07-20 06:27:14 -0700

    A function that prints 10 times the numbers, from 0 to 14, followed by a new line.

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,20 +7,15 @@
 void more_numbers(void)
 {
 	int i, j;
-
 	for (j = 0; j < 10; j++)
 	{
-		for (i = 0; i < 15; i++)
+		for (i = 0; i <= 14; i++)
 		{
-			if (i < 10)
-			{
-				_putchar(i + '0');
-			}
-			else
+			if (i >= 10)
 			{
 				_putchar(i / 10 + '0');
-				_putchar(i % 10 + '0');
 			}
+			_putchar(i % 10 + '0');
 		}
 		_putchar('\n');
 	}
