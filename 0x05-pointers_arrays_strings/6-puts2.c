90b150d75ad61a0f117379516b8b185f7c3633c7
Lance Desk <lncedesk.com>
2023-07-24 05:39:38 -0700

    A function that prints every other character of a string, starting with the first character, followed by a new line.

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,13 +7,16 @@
 
 void puts2(char *str)
 {
+	int position = 0;
+
 	while (*str != '\0')
 	{
-		if (*str % 2 == 0)
+		if (position % 2 == 0)
 		{
 			_putchar(*str);
 		}
 	str++;
+	position++;
 	}
 	_putchar('\n');
 }
