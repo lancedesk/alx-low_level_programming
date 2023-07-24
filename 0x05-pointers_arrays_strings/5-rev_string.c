9795a8ae6c858be32d812bf7e527dfe68b4b43f5
Lance Desk <lncedesk.com>
2023-07-24 05:00:20 -0700

    A function that reverses a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,16 +7,16 @@
 
 void rev_string(char *s)
 {
-		int len = strlen(s);
-		char *start = s;
-		char *end = s + len - 1;
+	int len = strlen(s);
+	char *start = s;
+	char *end = s + len - 1;
 
-		while (start < end)
-		{
-				char temp = *start;
-				*start  = *end;
-				*end = temp;
-				start++;
-				end--;
-		}
+	while (start < end)
+	{
+		char temp = *start;
+		*start  = *end;
+		*end = temp;
+		start++;
+		end--;
+	}
 }
