f3e4beebad48032bfb4b6bf722882a84180e2db4
Lance Desk <lncedesk.com>
2023-07-24 03:13:15 -0700

    A function that prints a string, followed by a new line, to stdout.

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - prints string to stdout
+ * @str: string input
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,6 +4,9 @@
 #include <string.h>
 #include <stdio.h>
 
+
+int _putchar(char c);
+void _puts(char *str);
 int _strlen(char *s);
 void swap_int(int *a, int *b);
 void reset_to_98(int *n);
