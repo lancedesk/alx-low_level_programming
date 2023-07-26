e96e55e9465c7bedf20564b59480c4768f96f9e9
Lance Desk <lncedesk.com>
2023-07-26 01:26:27 -0700

    A function that reverses the content of an array of integers.

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,6 +5,7 @@
 #include <stdio.h>
 
 
+void reverse_array(int *a, int n);
 int _strcmp(char *s1, char *s2);
 char *_strncpy(char *dest, char *src, int n);
 char *_strncat(char *dest, char *src, int n);
