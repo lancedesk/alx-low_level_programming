b48dec546114a456a84a360ffe70562217a7d9f4
Lance Desk <lncedesk.com>
2023-07-26 04:57:41 -0700

    A function that prints a buffer.

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,7 +6,7 @@
 #include <ctype.h>
 #include <unistd.h>
 
-
+void print_buffer(char *b, int size);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_number(int n);
 int _putchar(char c);
