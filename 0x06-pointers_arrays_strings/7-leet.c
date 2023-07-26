42fd812c8c9f9f653f0512e941f349f556ab37a9
Lance Desk <lncedesk.com>
2023-07-26 03:49:33 -0700

    A function that encodes a string into 1337.

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * leet - replaces characters in string
+ * @str: array to whose characters to replace
+ * Return: returns the edited array
+ */
+
+char *leet(char *str)
+{
+	int i;
+	int length = strlen(str);
+	char leetMap[256] = {0};
+
+    leetMap['a'] = leetMap['A'] = '4';
+	leetMap['e'] = leetMap['E'] = '3';
+	leetMap['o'] = leetMap['O'] = '0';
+	leetMap['t'] = leetMap['T'] = '7';
+	leetMap['l'] = leetMap['L'] = '1';
+
+	for (i = 0; i < length; i++)
+	{
+		if (leetMap[(unsigned char)str[i]])
+		{
+			str[i] = leetMap[(unsigned char)str[i]];
+		}
+	}
+
+	return str;
+}
