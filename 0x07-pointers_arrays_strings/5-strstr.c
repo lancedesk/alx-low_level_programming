976c708bc9a8e68295775a49e31b50828963d289
Lance Desk <lncedesk.com>
2023-07-31 02:06:39 -0700

    A function that locates a substring.

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _strstr - function finds the first occurrence 
+ * _strstr - function finds the first occurrence
  * of the substring needle in the string haystack
- * @haystack: null terminated string 
+ * @haystack: null terminated string
  * array to search in
  * @needle: null terminated array to search
- * Return: a pointer to the beginning of the 
- * located substring, 
+ * Return: a pointer to the beginning of the
+ * located substring,
  * or NULL if the substring is not found.
  */
 
@@ -15,7 +15,7 @@ char *_strstr(char *haystack, char *needle)
 {
 	char *locatedstring = strstr(haystack, needle);
 
-	if(locatedstring != NULL)
+	if (locatedstring != NULL)
 	{
 		return (locatedstring);
 	}
