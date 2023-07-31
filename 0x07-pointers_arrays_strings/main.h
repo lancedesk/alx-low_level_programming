f4c5de2f4be04bb824a0b7aecf2a53243774133d
Lance Desk <lncedesk.com>
2023-07-31 02:01:13 -0700

    A function that prints the chessboard.

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <string.h>
 
-
+int _putchar(char c);
 void set_string(char **s, char *to);
 void print_diagsums(int *a, int size);
 void print_chessboard(char (*a)[8]);
