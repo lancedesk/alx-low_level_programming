ad9daca62c619bd2f6fb944120b2d80a929ba0c7
Lance Desk <lncedesk.com>
2023-08-02 04:32:29 -0700

    A function that returns 1 if the input integer is a prime number, otherwise return 0.

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -11,15 +11,15 @@
 int is_prime_helper(int n, int i)
 {
 	if (n <= 1)
-		return 0;
+		return (0);
 
 	if (i * i > n)
-		return 1;
+		return (1);
 
 	if (n % i == 0)
-		return 0;
+		return (0);
 
-	return is_prime_helper(n, i + 1);
+	return (is_prime_helper(n, i + 1));
 }
 
 /**
@@ -31,5 +31,5 @@ int is_prime_helper(int n, int i)
 
 int is_prime_number(int n)
 {
-	return is_prime_helper(n, 2);
+	return (is_prime_helper(n, 2));
 }
