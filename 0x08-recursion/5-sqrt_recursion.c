7dae20d13699579c6aca0f98c98bb3a508d6b23d
Lance Desk <lncedesk.com>
2023-08-02 03:51:35 -0700

    A function that returns the natural square root of a number.

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,19 +3,66 @@
 /**
  * _sqrt_recursion - calculates square root of n
  * @n: number whose square root is to be found
+ * @low: low
+ * @high: high
  *
  * Return: square root of n
  */
 
+int _sqrt_recursive_helper(int n, int low, int high);
+
 int _sqrt_recursion(int n)
 {
-	double squareRoot = pow(n, 0.5);
-	int truncatedSquareRoot = (int)squareRoot;
-
-	if ((truncatedSquareRoot * truncatedSquareRoot) == n)
+	if (n < 0)
 	{
-		return (truncatedSquareRoot);
+		/* Square root of a negative number is not defined. */
+		return (-1);
+	}
+	else if (n == 0 || n == 1)
+	{
+		/* Base case: Square root of 0 or 1 is the number itself. */
+		return (n);
+	}
+
+	/* Call a helper function with a range from 1 to n/2. */
+	return (_sqrt_recursive_helper(n, 1, n / 2));
+}
+
+/**
+ * _sqrt_recursive_helper - helps calculate sqrt
+ * @n: number whose square root is to be found
+ * @low: low
+ * @high: high
+ * Return: square root of n
+ */
+
+int _sqrt_recursive_helper(int n, int low, int high)
+{
+	int mid, square;
+
+	if (low > high)
+	{
+		/* If low becomes greater than high, square root not found. */
+		return (-1);
+	}
+
+	/* Calculate the middle value. */
+	mid = low + (high - low) / 2;
+	square = mid * mid;
+
+	if (square == n)
+	{
+		/* If the square of the middle is equal to n, we found the square root. */
+		return (mid);
+	}
+	else if (square < n)
+	{
+		/* If the square of the middle is less than n, search the right half. */
+		return (_sqrt_recursive_helper(n, mid + 1, high));
 	}
 	else
-	return (-1);
+	{
+		/* If the square of the middle is greater than n, search the left half. */
+		return (_sqrt_recursive_helper(n, low, mid - 1));
+	}
 }
