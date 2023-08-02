a47c75f182ffc5ee8dcb32e5f51b405fa262c604
Lance Desk <lncedesk.com>
2023-08-02 01:34:28 -0700

    A function that returns the length of a string.

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - calculates length of string
+ * @s: null terminated string
+ * Return: the length of the null terminated string
+ */
+
+int _strlen_recursion(char *s)
+{
+	return (strlen(s));
+}
