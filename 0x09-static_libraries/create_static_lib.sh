156a55818ffa5db43d8f43c916b711e9e22426ce
Lance Desk <lncedesk.com>
2023-08-04 05:25:13 -0700

    A script called create_static_lib.sh that creates a static library called liball.a from all the .c files that are in the current directory.

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -3,5 +3,5 @@
 # Compile all .c files (excluding 'main.c' and 'libmy.c') into .o files
 find . -maxdepth 1 -type f \( -name '*.c' ! -name 'libmy.c' ! -name 'main.c' \) -exec gcc -c {} \;
 
-# Create the static library 'liball.a' from all .o files (excluding 'libmy.o')
-find . -maxdepth 1 -type f \( -name '*.o' ! -name 'libmy.o' ! -name 'main.o' ! -name '_puts.o' \) -exec ar rcs liball.a {} \;
+# Create the static library 'liball.a' with the desired order of .o files
+ar rcs liball.a 0-isupper.o 0-memset.o 0-strcat.o 100-atoi.o 1-isdigit.o 1-memcpy.o 1-strncat.o 2-strchr.o 2-strlen.o 2-strncpy.o 3-islower.o 3-puts.o 3-strcmp.o 3-strspn.o 4-isalpha.o 4-strpbrk.o 5-strstr.o 6-abs.o 9-strcpy.o _putchar.o
