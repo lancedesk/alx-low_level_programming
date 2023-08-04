d623390440826d55c6c8ce1adf3719bae79bc604
Lance Desk <lncedesk.com>
2023-08-04 07:51:17 -0700

    A program that multiplies two numbers.

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - entry point of the program
+ * @argc: command line argument count
+ * @argv: command line argument array
+ * Return: returns 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+
+		printf("%d\n", num1 * num2);
+	}
+	return 0;
+}
