42a90b58697522cfbcc75dd22b451f9dec5547e2
Lance Desk <lncedesk.com>
2023-08-07 00:53:15 -0700

    A function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,32 +1,40 @@
 #include "main.h"
 
 /**
- * _strdup - returns a pointer to a newly allocated
- * space in memory which contains a copy of the string
- * given as a parameter.
- *
- * @str: pointer to string
- * Return: returns NULL if str = NULL
- */
+  * _strdup - returns a pointer to a newly allocated
+  * space in memory which contains a copy of the string
+  * given as a parameter.
+  *
+  * @str: pointer to string
+  * Return: returns NULL if str = NULL
+  */
 
 char *_strdup(char *str)
 {
 	char *newstring;
 	size_t size, i;
 
-	size = strlen(str);
-	newstring = (char *)(malloc((size + 1) * sizeof(char)));
-
-	if (newstring == NULL || size == 0)
+	/* Return NULL early if str is NULL */
+	if (str == NULL)
 	{
 		return (NULL);
 	}
 
-	for (i = 0; i < size; i++)
+	size = strlen(str);
+	newstring = (char *)(malloc((size + 1) * sizeof(char)));
+
+	if (newstring == NULL || str[size] != '\0')
 	{
-		newstring[i] = str[i];
+		return (NULL);
+	}
+	else if (str[size] == '\0')
+	{
+		for (i = 0; i < size; i++)
+		{
+			newstring[i] = str[i];
+		}
 	}
 
-	newstring[size] = '\0'; /* Null-terminate the new string */
 	return (newstring);
+	newstring[size] = '\0'; /* Null-terminate the new string */
 }
