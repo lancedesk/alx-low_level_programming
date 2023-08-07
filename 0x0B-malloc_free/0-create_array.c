7b6d252032cb871cc2a0e0e1a3be9c4b7fa35150
Lance Desk <lncedesk.com>
2023-08-06 23:32:10 -0700

    Malloc in C programming language

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * create_array - creates an array of chars,
+ * and initializes it with a specific char.
+ * @size: size of array
+ * @c: specific char to be initialized
+ * Return: pointer to the array
+ * or Null if it fails
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *pointer;
+	int i;
+
+	pointer = (char *)(malloc(size * sizeof(char)));
+
+	if (pointer == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < (int)size; i++)
+	{
+		pointer[i] = c;    
+	}
+
+	return (pointer);
+
+	free(pointer);
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *create_array(unsigned int size, char c);
+
+#endif
