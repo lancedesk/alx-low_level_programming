f7d5d2d1f2ea02f50a6df211fba0449620a3b7d0
Lance Desk <lncedesk.com>
2023-08-10 04:57:05 -0700

    A function that concatenates two strings.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,44 +10,36 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i, size1, size2, totalsize, index = 0;
-	char *newstring, *tempstring;
+	unsigned int i, size1, size2, index = 0;
+	char *newstring;
 
 	size1 = (s1 != NULL) ? strlen(s1) : 0;
 	size2 = (s2 != NULL) ? strlen(s2) : 0;
-	totalsize = size1 + (((int)n >= size2) ? size2 : (int)n);
-	tempstring  = (char *)(malloc(totalsize * sizeof(char)));
 
-	if (tempstring == NULL)
-	{
-		return (NULL);
-	}
+	if (n >= size2)
+		/* Limit n to size2 to avoid unnecessary overallocation */
+		n = size2;
 
-	newstring = malloc((size1 + n) * sizeof(char));
+	newstring = malloc((size1 + n + 1) * sizeof(char));
 	if (newstring == NULL)
 	{
-		free(tempstring);
 		return (NULL);
 	}
 
 	for (i = 0; i < size1; i++)
 	{
-		tempstring[i] = s1[i];
+		newstring[index] = s1[i];
 		index++;
 	}
 
-	for (i = 0; i < size2; i++)
+	/* Only iterate up to n */
+	for (i = 0; i < n; i++)
 	{
-		tempstring[index] = s2[i];
+		newstring[index] = s2[i];
 		index++;
 	}
 
-	for (i = 0; i < (size1 + (int)n); i++)
-	{
-		newstring[i] = tempstring[i];
-		index++;
-	}
 	newstring[index] = '\0';
-	free(tempstring);
 	return (newstring);
 }
+
