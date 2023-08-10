3fe12610e91bfffbcb892c971084c09290f39e08
Lance Desk <lncedesk.com>
2023-08-10 03:03:57 -0700

    More C programming language malloc, exit, realloc, calloc functions

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * malloc_checked - returns a pointer to the allocated memory
+ * @b: number of bytes to allocate.
+ * Return: pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *pointer;
+
+	pointer = malloc(b);
+
+	if (pointer == NULL)
+	{
+		exit (98);
+	}
+
+	return (pointer);
+}
