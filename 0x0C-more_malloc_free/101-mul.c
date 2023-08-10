eaae63d8512ac81b84748638008c301f356d9224
Lance Desk <lncedesk.com>
2023-08-10 10:45:49 -0700

    A program that multiplies two positive numbers.

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,107 +1,156 @@
 #include "main.h"
 
 /**
- * main - multiplies two positive numbers
- * @argc: number of arguments
- * @argv: argument vectors
- * Return: 0 on success
- **/
-
-int main(int argc, char *argv[])
+ * str_len - finds string length
+ * @str: input pointer to string
+ * Return: length of string
+ */
+int str_len(char *str)
 {
-	char *num1_str, *num2_str;
+	int len;
 
-	if (argc != 3 || !onlyNumbers(argv[1]) || !onlyNumbers(argv[2]))
+	for (len = 0; *str != '\0'; len++)
+		len++, str++;
+	return (len / 2);
+}
+/**
+ * _calloc - allocates memory for an array using malloc
+ * @bytes: bytes of memory needed per size requested
+ * @size: size in bytes of each element
+ * Return: pointer to the allocated memory
+ */
+void *_calloc(unsigned int bytes, unsigned int size)
+{
+	unsigned int i;
+	char *p;
+
+	if (bytes == 0 || size == 0)
+		return (NULL);
+	if (size >= UINT_MAX / bytes || bytes >= UINT_MAX / size)
+		return (NULL);
+	p = malloc(size * bytes);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < bytes * size; i++)
+		p[i] = 0;
+	return ((void *)p);
+}
+/**
+ * add_arrays - adds 2 arrays of ints
+ * @mul_result: pointer to array with numbers from product
+ * @sum_result: pointer to array with numbers from total sum
+ * @len_r: length of both arrays
+ * Return: void
+ */
+void add_arrays(int *mul_result, int *sum_result, int len_r)
+{
+	int i = 0, len_r2 = len_r - 1, carry = 0, sum;
+
+	while (i < len_r)
 	{
-		printf("Error\n");
-		return (98);
+		sum = carry + mul_result[len_r2] + sum_result[len_r2];
+		sum_result[len_r2] = sum % 10;
+		carry = sum / 10;
+		i++;
+		len_r2--;
 	}
-
-	num1_str = argv[1];
-	num2_str = argv[2];
-
-	multiplyAndPrint(num1_str, num2_str);
-
+}
+/**
+ * is_digit - checks for digits
+ * @c: input character to check for digit
+ * Return: 0 failure, 1 success
+ */
+int is_digit(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	printf("Error\n");
 	return (0);
 }
-
 /**
- * multiplyAndPrint - multiplies two positive numbers and prints the result
- * @num1_str: first positive number as a string
- * @num2_str: second positive number as a string
- **/
-void multiplyAndPrint(char *num1_str, char *num2_str)
+ * multiply - multiplies 2 #'s, prints result, must be 2 #'s
+ * @num1: factor # 1 (is the smaller of 2 numbers)
+ * @len_1: length of factor 1
+ * @num2: factor # 2 (is the larger of 2 numbers)
+ * @len_2: length of factor 2
+ * @len_r: length of result arrays
+ * Return: 0 fail, 1 success
+ */
+int *multiply(char *num1, int len_1, char *num2, int len_2, int len_r)
 {
-	int len1 = _strlen(num1_str);
-	int len2 = _strlen(num2_str);
-	int total_len = len1 + len2;
-	int *result = calloc(total_len, sizeof(int));
-	int start, i, j, carry, digit1, digit2, temp;
+	int i = 0, i1 = len_1 - 1;
+	int i2, product, carry, digit, *mul_result, *sum_result;
 
-	if (!result)
+	sum_result = _calloc(sizeof(int), (len_r));
+	while (i < len_1)
+	{
+		mul_result = _calloc(sizeof(int), len_r);
+		i2 = len_2 - 1, digit = (len_r - 1 - i);
+		if (!is_digit(num1[i1]))
+			return (NULL);
+		carry = 0;
+		while (i2 >= 0)
+		{
+			if (!is_digit(num2[i2]))
+				return (NULL);
+			product = (num1[i1] - '0') * (num2[i2] - '0');
+			product += carry;
+			mul_result[digit] += product % 10;
+			carry = product / 10;
+			digit--, i2--;
+		}
+		add_arrays(mul_result, sum_result, len_r);
+		free(mul_result);
+	    i++, i1--;
+	}
+	return (sum_result);
+}
+/**
+ * print_me - prints my array of the hopeful product here
+ * @sum_result: pointer to int array with numbers to add
+ * @len_r: length of result array
+ * Return: void
+ */
+void print_me(int *sum_result, int len_r)
+{
+	int i = 0;
+
+	while (sum_result[i] == 0 && i < len_r)
+		i++;
+	if (i == len_r)
+		_putchar('0');
+	while (i < len_r)
+		_putchar(sum_result[i++] + '0');
+	_putchar('\n');
+}
+/**
+ * main - multiply 2 input #'s of large lengths and print result or print Error
+ * @argc: input count of args
+ * @argv: input array of string args
+ * Return: 0, Success
+ */
+int main(int argc, char **argv)
+{
+	int len_1, len_2, len_r, temp, *sum_result;
+	char *num1, *num2;
+
+	if (argc != 3)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
-	for (i = len1 - 1; i >= 0; i--)
+	len_1 = str_len(argv[1]), len_2 = str_len(argv[2]);
+	len_r = len_1 + len_2;
+	if (len_1 < len_2)
+		num1 = argv[1], num2 = argv[2];
+	else
 	{
-		carry = 0;
-		digit1 = num1_str[i] - '0';
-
-		for (j = len2 - 1; j >= 0; j--)
-		{
-			digit2 = num2_str[j] - '0';
-			temp = result[i + j + 1] + (digit1 * digit2) + carry;
-
-			result[i + j + 1] = temp % 10;
-			carry = temp / 10;
-		}
-
-		if (carry)
-		{
-			result[i] += carry;
-		}
+		num1 = argv[2], num2 = argv[1];
+		temp = len_2, len_2 = len_1, len_1 = temp;
 	}
-
-	start = (result[0] == 0) ? 1 : 0;
-
-	for (i = start; i < total_len; i++)
-	{
-		printf("%d", result[i]);
-	}
-	printf("\n");
-	free(result);
+	sum_result = multiply(num1, len_1, num2, len_2, len_r);
+	if (sum_result == NULL)
+		exit(98);
+	print_me(sum_result, len_r);
+	return (0);
 }
-
-/**
- * onlyNumbers - checks if a string has only numbers
- * @c: input string
- * Return: 1 if true, 0 if false
- **/
-int onlyNumbers(char *c)
-{
-	while (*c)
-	{
-		if (*c < '0' || *c > '9')
-			return (0);
-		c++;
-	}
-	return (1);
-}
-
-/**
- * _strlen - returns the length of a string
- * @s: string s
- * Return: length of string
- **/
-int _strlen(char *s)
-{
-	int length = 0;
-
-	while (s[length] != '\0')
-		length++;
-
-	return (length);
-}
-
