0eebf9133b38f6ef53891c88507a1a3eb4c4d40e
Lance Desk <lncedesk.com>
2023-08-16 03:16:43 -0700

    A function that searches for an integer.

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -3,36 +3,31 @@
 /**
  * int_index - searches for an integer
  * @array: array to search
- * @size: number of elements in array
+ *@size: number of elements in array
  * @cmp: function to compare values
- * Return: returns index or -1 if not found
-*/
+ */
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i, index = 0;
 
-	if (size <= 0 || array == NULL || cmp == NULL)
+	if (size > 0 && cmp && size && array)
 	{
-		return (-1);
-	}
 
-	for (i = 0; i <= size; i++)
-	{
-		if (cmp(array[i]) != 0)
+		for (i = 0; i < size; i++)
 		{
-			break;
+			if (cmp(array[i]) != 0)
+			{
+				break;
+			}
+			index++;
 		}
-		index++;
+
+		if (index >= 0)
+		{
+			return (index);
+		}
 	}
 
-	if (index >= 0)
-	{
-		return (index);
-	}
-
-	else
-	{
-		return (-1);
-	}
+	return (-1);
 }
