9e44b504cf3ff192a183353e1b7bb4ce2dcc5c5d
Lance Desk <lncedesk.com>
2023-08-16 02:22:07 -0700

    A function that prints a name.

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,26 +5,13 @@
  * @name: name to print
  * @f: function as parameter
  */
-
 void print_name(char *name, void (*f)(char *))
 {
-	void (*name_as_is)(char *);
-	void (*in_caps)(char *);
-
 	if (name == NULL || f == NULL)
 	{
 		return;
 	}
 
-	if (f == print_name_as_is)
-	{
-		name_as_is = print_name_as_is;
-		name_as_is(name);
-	}
-
-	else
-	{
-		in_caps = print_name_uppercase;
-		in_caps(name);
-	}
+	f(name);
 }
+
