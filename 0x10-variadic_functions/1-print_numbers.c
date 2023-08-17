ebc4db14e6d0ff92f81d0787430acd821d32853e
Lance Desk <lncedesk.com>
2023-08-17 04:10:32 -0700

    A function that prints numbers, followed by a new line.

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,20 +13,19 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list numbers;
 
-	if (separator == NULL)
-	{
-		exit(98);
-	}
-
 	va_start(numbers, n);
 
 	for (i = 0; i < n; i++)
 	{
-		if (i != n - 1)
+		if (i != n - 1 && separator != NULL)
 		{
 			printf("%d%s", va_arg(numbers, int), separator);
 		}
-		else if (i == n - 1)
+		else if (i == n - 1 && separator != NULL)
+		{
+			printf("%d", va_arg(numbers, int));
+		}
+		else
 		{
 			printf("%d", va_arg(numbers, int));
 		}
