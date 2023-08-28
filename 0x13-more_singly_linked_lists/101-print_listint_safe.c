a5b6ad9e9691488b7e22e2ca94d8493e218e25e6
Lance Desk <lncedesk@gmail.com>
2023-08-28 12:07:55 -0700

    A function that prints a listint_t linked list.

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,44 +1,10 @@
 #include "lists.h"
 
-/**
- * allocate_memory - Reallocates memory for an array of pointers
- * to nodes in a linked list.
- * @list: Pointer to the old array of pointers.
- * @size: Size of the new array (one more than the old size).
- * @new: Pointer to the new node to be added.
- *
- * This function reallocates memory for a new array of pointers, copies the old
- * pointers to the new array, adds the new pointer to the end, and returns the
- * new array. If memory allocation fails, it frees the old array and exits with
- * status 98.
- *
- * Return: Pointer to the new array of pointers.
- */
-
 const listint_t **allocate_memory(
 		const listint_t **list,
 		size_t size,
 		const listint_t *new
-		)
-{
-	const listint_t **newlist;
-	size_t i;
-
-	newlist = malloc(size * sizeof(listint_t *));
-	if (newlist == NULL)
-	{
-		free(list);
-		exit(98);
-	}
-
-	for (i = 0; i < size - 1; i++)
-	{
-		newlist[i] = list[i];
-	}
-	newlist[size - 1] = new;
-
-	return (newlist);
-}
+		);
 
 /**
  * print_listint_safe - Prints a listint_t linked list safely.
@@ -55,30 +21,62 @@ const listint_t **allocate_memory(
 
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t count = 0, i;
-	const listint_t **list = NULL;
+	size_t i, count = 0;
+	const listint_t **printed_list = NULL;
 
 	while (head != NULL)
 	{
 		for (i = 0; i < count; i++)
 		{
-			if (head == list[i])
+			if (head == printed_list[i])
 			{
 				printf("-> [%p] %d\n", (void *)head, head->n);
-				free(list);
-
+				free(printed_list);
 				return (count);
 			}
 		}
-
 		count++;
-		list = allocate_memory(list, count, head);
-
+		printed_list = allocate_memory(printed_list, count, head);
 		printf("[%p] %d\n", (void *)head, head->n);
 		head = head->next;
 	}
-
-	free(list);
+	free(printed_list);
 	return (count);
 }
 
+/**
+ * allocate_memory - Reallocates memory for an array of pointers
+ * to nodes in a linked list.
+ * @printed_list: Pointer to the old array of pointers.
+ * @size: Size of the new array (one more than the old size).
+ * @new: Pointer to the new node to be added.
+ *
+ * This function reallocates memory for a new array of pointers, copies the old
+ * pointers to the new array, adds the new pointer to the end, and returns the
+ * new array. If memory allocation fails, it frees the old array and exits with
+ * status 98.
+ *
+ * Return: Pointer to the new array of pointers.
+ */
+
+const listint_t **allocate_memory(
+		const listint_t **printed_list,
+		size_t size,
+		const listint_t *new
+		)
+{
+	const listint_t **new_list;
+	size_t i;
+
+	new_list = malloc(size * sizeof(listint_t *));
+	if (new_list == NULL)
+	{
+		free(printed_list);
+		exit(98);
+	}
+	for (i = 0; i < size - 1; i++)
+		new_list[i] = printed_list[i];
+	new_list[i] = new;
+	free(printed_list);
+	return (new_list);
+}
