4c3c55855373615681baf4d8c898aa58873b3d6d
Lance Desk <lncedesk@gmail.com>
2023-08-28 00:01:12 -0700

    A function that frees a listint_t list.

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Frees a list of integers
+ * and sets the head to NULL.
+ * @head: Pointer to the head of the list.
+ *
+ * This function deallocates the memory
+ * occupied by each node
+ * in the given linked list of integers
+ * and sets the head to NULL.
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *freed_list;
+
+	while (*head != NULL)
+	{
+		freed_list = *head;
+		*head = (*head)->next;
+		free(freed_list);
+	}
+}
