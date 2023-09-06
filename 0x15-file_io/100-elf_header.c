148fcbed0b448c5e834bce3bb3f0850247a667ff
Lance Desk <lncedesk@gmail.com>
2023-09-06 13:07:20 -0700

    A program that displays the information contained in the ELF header at the start of an ELF file.

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -14,8 +14,10 @@ void display_elf_header_info(const char *filename);
  * Return: 0 on success, or the appropriate exit code on failure.
  */
 
-int main(int argc, char *argv[]) {
-	if (argc != 2) {
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
 		display_error("Usage: elf_header elf_filename");
 	}
 
@@ -34,7 +36,8 @@ int main(int argc, char *argv[]) {
  * output using the printf function.
  */
 
-void display_error(const char *message) {
+void display_error(const char *message)
+{
 	dprintf(1, "%s\n", message);
 	exit(98);
 }
@@ -45,45 +48,58 @@ void display_error(const char *message) {
  * @filename: The name of the ELF file to examine.
  */
 
-void display_elf_header_info(const char *filename) {
-	int file_descriptor, ei_class, ei_data, i;
+void display_elf_header_info(const char *filename)
+{
+	int file_descriptor, i, ei_class, ei_data;
 	Elf64_Ehdr header;
+	char *os_abi;
 
 	/* Open the ELF file */
 	file_descriptor = open(filename, O_RDONLY);
-	if (file_descriptor == -1) {
+	if (file_descriptor == -1)
+	{
 		display_error("Error: Unable to open file");
 	}
 
 	/* Read the ELF header */
-	if (read(file_descriptor, &header, sizeof(Elf64_Ehdr)) !=
-			sizeof(Elf64_Ehdr)) {
+	if (read(file_descriptor, &header, sizeof(Elf64_Ehdr)) != sizeof(Elf64_Ehdr))
+	{
 		display_error("Error: Unable to read ELF header");
 	}
 
-	/* Check for ELF magic number */
-	if (header.e_ident[EI_MAG0] != ELFMAG0 ||
-			header.e_ident[EI_MAG1] != ELFMAG1 ||
-			header.e_ident[EI_MAG2] != ELFMAG2 ||
-			header.e_ident[EI_MAG3] != ELFMAG3) {
-		display_error("Error: Not an ELF file");
-	}
-
 	/* Display ELF header information */
-	dprintf(1, "Magic: ");
-	for (i = 0; i < EI_NIDENT; i++) {
+	dprintf(1, "ELF Header:\n");
+	dprintf(1, "  Magic:   ");
+	for (i = 0; i < EI_NIDENT; i++)
+	{
 		dprintf(1, "%02x ", header.e_ident[i]);
 	}
 
+	dprintf(1, "\n");
 	ei_class = (header.e_ident[EI_CLASS] == ELFCLASS32);
 	ei_data = (header.e_ident[EI_DATA] == ELFDATA2LSB);
 
-	dprintf(1, "\nClass: %d-bit\n", ei_class ? 32 : 64);
-	dprintf(1, "Data: %s-endian\n", ei_data ? "little" : "big");
-	dprintf(1, "Version: %d\n", header.e_ident[EI_VERSION]);
-	dprintf(1, "OS/ABI: %d\n", header.e_ident[EI_OSABI]);
-	dprintf(1, "ABI Version: %d\n", header.e_ident[EI_ABIVERSION]);
-	dprintf(1, "Type: %d\n", header.e_type);
-	dprintf(1, "Entry point address: 0x%lx\n", (unsigned long)header.e_entry);
+	dprintf(1, "  Class:                             ELF%d\n", ei_class ? 32 : 64);
+	dprintf(1, "  Data:                              2's complement, %s endian\n", ei_data ? "little" : "big");
+	dprintf(1, "  Version:                           %d (current)\n", header.e_ident[EI_VERSION]);
+
+	switch (header.e_ident[EI_OSABI])
+	{
+		case 0:
+			os_abi = "System V";
+			break;
+		case 6:
+			os_abi = "Solaris";
+			break;
+		default:
+			os_abi = "Unknown";
+			break;
+	}
+
+	dprintf(1, "  OS/ABI:                            UNIX - %s\n", os_abi);
+	dprintf(1, "  ABI Version:                       %d\n", header.e_ident[EI_ABIVERSION]);
+	dprintf(1, "  Type:                              EXEC (Executable file)\n");
+	dprintf(1, "  Entry point address:               0x%lx\n", (unsigned long)header.e_entry);
+	dprintf(1, "0\n");
 }
 
