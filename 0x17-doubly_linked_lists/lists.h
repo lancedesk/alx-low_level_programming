9a8622670e41b66a0cebe12a31cb6e55596238fd
Lance Desk <lncedesk@gmail.com>
2023-10-12 02:34:54 -0700

    C programming language doubly linked lists

---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,33 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ *
+*/
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stddef.h>
+
+
+typedef struct dlistint_s
+{
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
+
+
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+size_t dlistint_len(const dlistint_t *h);
+size_t print_dlistint(const dlistint_t *h);
+
+#endif
