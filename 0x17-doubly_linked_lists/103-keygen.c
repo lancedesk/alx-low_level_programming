4f0a5fe75ff7ce2a2156a1a2caa75edffd81b958
Lance Desk <lncedesk@gmail.com>
2023-10-12 04:29:04 -0700

    A keygen for crackme5.

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -9,24 +9,24 @@
 
 int main(int argc, char *argv[])
 {
-	char key[7], *char_array;
+	char key[7], *chars;
 	int length = strlen(argv[1]), i, temp;
 
 	(void)argc;
-	char_array = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+	chars = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 
 	temp = (length ^ 59) & 63;
-	key[0] = char_array[temp];
+	key[0] = chars[temp];
 
 	temp = 0;
 	for (i = 0; i < length; i++)
 		temp += argv[1][i];
-	key[1] = char_array[(temp ^ 79) & 63];
+	key[1] = chars[(temp ^ 79) & 63];
 
 	temp = 1;
 	for (i = 0; i < length; i++)
 		temp *= argv[1][i];
-	key[2] = char_array[(temp ^ 85) & 63];
+	key[2] = chars[(temp ^ 85) & 63];
 
 	temp = 0;
 	for (i = 0; i < length; i++)
@@ -35,16 +35,16 @@ int main(int argc, char *argv[])
 			temp = argv[1][i];
 	}
 	srand(temp ^ 14);
-	key[3] = char_array[rand() & 63];
+	key[3] = chars[rand() & 63];
 
 	temp = 0;
 	for (i = 0; i < length; i++)
 		temp += (argv[1][i] * argv[1][i]);
-	key[4] = char_array[(temp ^ 239) & 63];
+	key[4] = chars[(temp ^ 239) & 63];
 
 	for (i = 0; i < argv[1][0]; i++)
 		temp = rand();
-	key[5] = char_array[(temp ^ 229) & 63];
+	key[5] = chars[(temp ^ 229) & 63];
 
 	key[6] = '\0';
 	printf("%s", key);
